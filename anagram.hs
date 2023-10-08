4768799406fe7d74b847faf640c7c61bea7fe259
jonas <jonasmpdc@gmail.com>
2023-10-08 19:03:56 -0300

    simplified anagram

--- anagram.hs
+++ anagram.hs
@@ -3,24 +3,13 @@ module Anagram (anagramsFor) where
 import Data.Char
 import Data.List (sort)
 
-validAlphabeticCharacters :: [Char]
-validAlphabeticCharacters = ['A' .. 'Z'] ++ ['a' .. 'z']
-
-subject :: String
-subject = "orchestra"
-
-candidates :: [String]
-candidates = ["cashregister", "Carthorse", "radishes"]
-
 stringToUpper :: String -> String
 stringToUpper = map toUpper
 
 isAnagram :: String -> String -> Bool
-isAnagram x y = not isSameWord && not isEmpty && hasSameLength && isAnagram
+isAnagram x y = not isSameWord && isAnagram
   where
-    hasSameLength = length x == length y
     isSameWord = stringToUpper x == stringToUpper y
-    isEmpty = null x
     isAnagram = sort (stringToUpper x) == sort (stringToUpper y)
 
 anagramsFor :: String -> [String] -> [String]
