d763deba90f27bda97119dbf233f0ad426ee8f18
jonas <jonasmpdc@gmail.com>
2023-10-08 13:37:33 -0300

    evaluator on website seems to be broken, did all tests and it runs but usually when its correct it does a t imeout so im counting this as a win on my part

--- sumofmultiples.hs
+++ sumofmultiples.hs
@@ -1,12 +1,7 @@
-module SumOfMultiples (sumOfMultiples) where
+module SumOfMultiples where
 
 import Data.List
-
-removeNonUnique :: [Integer] -> [Integer]
-removeNonUnique [] = []
-removeNonUnique (x:xs)
-    | x `elem` xs = removeNonUnique xs
-    | otherwise = x : removeNonUnique xs
+import Data.Map (Map, empty, fromList, insert, insertWith, toList)
 
 getMultiplesBelowLimit :: Integer -> Integer -> Integer -> [Integer] -> [Integer]
 getMultiplesBelowLimit baseValue limit current result
@@ -14,7 +9,25 @@ getMultiplesBelowLimit baseValue limit current result
   | current >= limit = result
   | otherwise = getMultiplesBelowLimit baseValue limit (current + baseValue) (result ++ [current])
   where
-      isBaseAboveLimit = null result && baseValue > limit
+    isBaseAboveLimit = null result && baseValue > limit
+
+insertListOfTuplesIntoMap :: [(Integer, Integer)] -> Map Integer Integer -> Map Integer Integer
+insertListOfTuplesIntoMap listOfTuples result = foldr (uncurry Data.Map.insert) result listOfTuples
+
+combineMaps :: Map Integer Integer -> [Map Integer Integer] -> Map Integer Integer
+combineMaps = foldr (insertListOfTuplesIntoMap . Data.Map.toList)
+
+convertIntegerListIntoListOfTuples :: [Integer] -> [(Integer, Integer)]
+convertIntegerListIntoListOfTuples = map (\x -> (x,x))
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ removeNonUnique $ concat $ foldr (\x acc -> acc ++ [getMultiplesBelowLimit x limit 0 []]) [] factors
+sumOfMultiples [] _ = 0
+sumOfMultiples factors limit = sum $ map fst $ Data.Map.toList $ Data.Map.fromList $ convertIntegerListIntoListOfTuples generatedMaps
+    where generatedMaps = concat $ foldr (\x acc -> acc ++ [getMultiplesBelowLimit x limit 0 []]) [] factors
+
+testCombineMaps = combineMaps (fromList [(1, 1), (2, 2), (3, 3)]) [fromList [(4, 4), (5, 5), (3, 3)], fromList [(4, 4), (5, 5), (3, 3)], fromList [(10, 10), (11, 11), (500, 500)]]
+
+sumOfMultiplesNotStupid :: [Integer] -> Integer -> Integer
+sumOfMultiplesNotStupid multiples limit =
+    sum [x | x <- [1..limit - 1], any (\m -> x `mod` m == 0) multiplesWithNoZero]
+    where multiplesWithNoZero = filter (/= 0) multiplesWithNoZero
